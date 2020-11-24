6a9929596f1d4f0b65055a567ba32e852bcfaa15
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-11-24 13:24:43 -0700

    pandas boy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import sys
 import os
 import csv
-from csvvalidator import *
+import petl as etl 
 
-print("dumb fuck")
+#print("dumb fuck")
+text = 'a,1\nb,2\nc,3\n'
+with open('example.txt', 'w') as f:
+    f.write(text)
+T1 = etl.fromtext('example.txt')
+T2 = T1.capture('lines','(.*),(.*)$',['Fucks','Given'])
+print(T2)
