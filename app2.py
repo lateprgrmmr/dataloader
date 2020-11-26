99fbf83cac6659a96c763452e968671af5b3b6cd
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-11-26 16:47:12 -0700

    added usaddress

---
+++ app2.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+browser.get('http://www.google.com/maps')
+
+search_term = 'Billings Funeral Home'
+
+search = browser.find_element_by_name('q')
+search.send_keys(search_term)
+search.send_keys(Keys.RETURN)
+
+#time.sleep(5)
+
---
+++ contacts.py
@@ -0,0 +1,5 @@
+import usaddress as us
+import pandas as pd
+
+p = pd.DataFrame(us.tag('1402 West Sandalwood Dr. Meridian, ID 83646'))
+p
