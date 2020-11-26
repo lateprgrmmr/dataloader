a2af87697117e4bab6a916f392a74517af8f0ff1
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-11-26 16:49:37 -0700

    trying

--- contacts.py
+++ contacts.py
@@ -1,5 +1,5 @@
 import usaddress as us
 import pandas as pd
 
-p = pd.DataFrame(us.tag('1402 West Sandalwood Dr. Meridian, ID 83646'))
-p
+p = pd.DataFrame(us.parse('1402 West Sandalwood Dr. Meridian, ID 83646'))
+print(p)
