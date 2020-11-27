ba7fe7d290089e44a42f7be48e28802a16f86692
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-11-27 01:17:54 -0700

    ugh

--- contacts.py
+++ contacts.py
@@ -1,15 +1,9 @@
 import usaddress as us
 import easypost as ep
 import pandas as pd
-import easypost
-import json
-import csv
 
 #easypost.api_key = "EZTKece6bf0ff0fa48bf8da13fa4196afd16f8dyDUj8fqj0AtdYj0jmuQ"
 
-with open('address.csv', 'r') as address:
-    for row in address:
-        a = us.parse(row)
-        print(a)
-
-
+Recipient = []
+addresses = []
+a = pd.read_csv('address.csv', index_col="Company ID")
