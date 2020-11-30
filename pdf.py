c166f9b6704e575402ec9a3d61c7c6f0f2f0669e
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-11-29 18:45:18 -0700

    playing with pdfs

--- pdf.py
+++ pdf.py
@@ -3,11 +3,17 @@ from pdfreader import PDFDocument, SimplePDFViewer
 
 fd = open('nomis_sample.pdf', 'rb')
 doc = PDFDocument(fd)
-viewer = SimplePDFViewer(fd)
-#viewer.navigate()
+#viewer = SimplePDFViewer(fd)
+# viewer.navigate()
 # print(viewer.render())
-print(viewer.render())
+
 # print(doc.header.version)
 # print(doc.root.Type)
 # print(doc.root.Metadata.Subtype)
 # print(doc.root.Outlines.First['Title'])
+print(pdfreader.types.objects.Page(doc))
+
+U = v.canvas.strings
+for each in U:
+    u = each.replace('.', '').replace('\n', '').replace('\b', '')
+    print(''.join(u))
