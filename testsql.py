103483666d3b4224f0b3756271c0191ebd8ec270
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-12-04 20:14:11 -0700

    fixed one column name that was fucking everything up...

---
+++ testsql.py
@@ -0,0 +1,33 @@
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost', database='scraping_sample', user='scraping_user', password='pword')
+c = db.cursor()
+# sql = "INSERT INTO classes(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(
+#     name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, 'NOW()')
+sql = "INSERT INTO classes(name_of_class,hours_per_week_in_term,expected_number_of_participants,maximum_participants,assignment,lecture_id,credit_points,language,created_at) VALUES('test',5,22,30,'test',1,3,'english',NOW())"
+#sql = "Select * from classes"
+c.execute(sql)
+db.commit()
+db.close()
+#a = c.fetchall()
+#print(a)
+
+CREATE TABLE `classes2` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `name_of_class` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `type_of_course` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `lecturer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `short_text` text COLLATE utf8_unicode_ci DEFAULT NULL,
+ `choice_term` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `hours_per_week_in_term` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `expected_num_of_participants` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `maximum_participants` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `assignment` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `lecture_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `credit_points` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `hyperlink` text COLLATE utf8_unicode_ci DEFAULT NULL,
+ `language` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `created_at` timestamp NULL DEFAULT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- web.py
+++ web.py
@@ -3,22 +3,24 @@ import MySQLdb
 from bs4 import BeautifulSoup
 
 # SQL connection
-HOST = 'localhost'
-USERNAME = 'scraping_user'
-PASSWORD = 'pword'
-DATABASE = 'scraping_sample'
+# HOST = 'localhost'
+# USERNAME = 'scraping_user'
+# PASSWORD = 'pword'
+# DATABASE = 'scraping_sample'
 
 # URL
 url_to_scrape = 'https://howpcrules.com/sample-page-for-web-scraping/'
 plain_html_text = requests.get(url_to_scrape)
 soup = BeautifulSoup(plain_html_text.text, 'html.parser')
+# print(soup.prettify())
 
 name_of_class = soup.h3.text.strip()
+print(name_of_class)
 
 basic_data_table = soup.find('table', {'summary': 'Basic data for the event'})
 basic_data_cells = basic_data_table.findAll('td')
 
-#get all the different data from the table's tds
+# get all the different data from the table's tds
 type_of_course = basic_data_cells[0].text.strip()
 lecturer = basic_data_cells[1].text.strip()
 number = basic_data_cells[2].text.strip()
@@ -33,7 +35,30 @@ credit_points = basic_data_cells[10].text.strip()
 hyperlink = basic_data_cells[11].text.strip()
 language = basic_data_cells[12].text.strip()
 
-dates_tables = soup.find_all('table', {'summary': 'Overview of all event dates'})
+db = MySQLdb.connect(host='localhost', database='scraping_sample', user='scraping_user', password='pword')
+# print('anus')
+cursor = db.cursor()
+# print('gum')
+sql = "INSERT INTO classes(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(
+    name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, 'NOW()')
+# print(sql)
+try:
+    cursor.execute(sql)
+    db.commit()
+except:
+    db.rollback()
+sql = "SELECT LAST_INSERT_ID()"
+try:
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    class_id = result[0]
+except:
+    db.rollback()
+    db.close()
+    class_id = -1
+
+dates_tables = soup.find_all(
+    'table', {'summary': 'Overview of all event dates'})
 for table in dates_tables:
     for row in table.select('tr'):
         cells = row.findAll('td')
@@ -41,12 +66,10 @@ for table in dates_tables:
             duration = cells[0].text.split('to')
             start_date = duration[0].strip()
             end_date = duration[1].strip()
-
             day = cells[1].text.strip()
             time = cells[2].text.strip('to')
             start_time = time[0].strip()
             end_time = time[1].strip()
-            
             frequency = cells[3].text.strip()
             room = cells[4].text.strip()
             lecturer_for_date = cells[5].text.strip()
@@ -54,3 +77,14 @@ for table in dates_tables:
             remarks = cells[7].text.strip()
             cancelled_on = cells[8].text.strip()
             max_participants = cells[9].text.strip()
+
+            db = MySQLdb.connect(host='localhost', database='scraping_sample', user='scraping_user', password='pword')
+            cursor = db.cursor()
+            sql = "INSERT INTO events(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(
+                class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, 'NOW()')
+            try:
+                cursor.execute(sql)
+                db.commit()
+            except:
+                db.rollback()
+                db.close()
