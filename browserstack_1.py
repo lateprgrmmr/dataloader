f9eff1de1595ac823cd0b15f56981c825207a58d
Kevin Bratt <kevinbratt@Kevins-Work-MacBook-Pro.local>
2020-12-25 15:50:12 -0700

    gettin it

--- browserstack_1.py
+++ browserstack_1.py
@@ -29,9 +29,12 @@ driver = webdriver.Chrome(options=options)
 #     desired_capabilities=desired_cap
 # )
 
+test_address = input("Address to test: ")
+ADDRESS_SPAN_STR = '//span[text()="View {0}\'s Dashboard"]'.format(test_address)
+
 driver.get("https://my.gatherqa.app/admin/")
-if "Google" in driver.title:
-    raise Exception("Unable to load google page!")
+if not "Gather" in driver.title:
+    raise Exception("Unable to load app!")
 login_name = driver.find_element_by_xpath('//*[@id="adornment-email"]')
 login_name.send_keys(USER)
 login_pword = driver.find_element_by_xpath('//*[@id="adornment-password"]')
@@ -40,17 +43,50 @@ login_submit = driver.find_element_by_xpath(
     '//*[@id="root"]/div/div[1]/div[2]/form/div[3]/div/button[2]')
 login_submit.click()
 wait = WebDriverWait(driver, 20)
-create_new_funeral_home = wait.until(EC.element_to_be_clickable((By.ID, 'common-drawer')))
-    # '//div[@id="common-drawer"]//*[name()="svg"]/*[name()="path"]')
-    # '//*[@id="common-drawer"]//*[name()="svg"]//*[name()="d"]')
-    # "//div[@id='common-drawer']//*[local-name()='svg']/*[*[local-name()='path']]")))
-create_new_funeral_home.click()
+create_wait = WebDriverWait(driver, 35)
+# -- Create New Funeral Home button
+create_new_funeral_home = wait.until(EC.element_to_be_clickable(
+    (By.XPATH, '//span[text()="  create New Funeral Home"]'))).click()
+add_new_funeral_home_address = driver.find_element_by_id(
+    "downshift-simple-input").send_keys(test_address)
+add_new_funeral_home_address_button = driver.find_element_by_xpath(
+    '//span[text()="Create Funeral Home"]').click()
+print(ADDRESS_SPAN_STR)
+complete_create = create_wait.until(EC.element_to_be_clickable(
+    (By.XPATH, ADDRESS_SPAN_STR))).click()
+# # -- Funeral Homes button
+# funeral_homes = wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Funeral Homes"]'))
+# # -- Cases button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Cases"]'))
+# # -- Live Stream button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Live Stream"]'))
+# # -- G&S button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Goods & Services"]'))
+# # -- Program Builder button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Program Builder"]'))
+# # -- Designs button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Designs"]'))
+# # -- Content Library button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Content Library"]'))
+# # -- Service Detail button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="Service Detail"]'))
+# -- My Team button
+# funeral_homes=wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="My Team"]'))).click()
+# # -- My Settings button
+# funeral_homes=wait.until(EC.element_to_be_clickable(By.XPATH, '//span[text()="My Settings"]'))
+# -- Invite new team member
+# invite_new_team_member = wait.until(EC.element_to_be_clickable(
+#     (By.XPATH, '//span[class()="jss1001"]'))).click()
 # elem.send_keys("BrowserStack")
 # elem.submit()
 print(driver.title)
 # driver.quit()
 
-# < button class = "jss906 jss880 jss882 jss883 jss885 jss886 jss1572" tabindex = "0" type = "button" >
-# < span class = "jss881" > < svg class = "jss169 jss1571" focusable = "false" viewBox = "0 0 24 24" aria - hidden = "true" role = "presentation" >
-# <path d = "M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" > </path > </svg > create New Funeral Home < /span > <span class = "jss909" > </span > </button >
-#common-drawer > div > div.jss1562.jss1634 > div > button.jss906.jss880.jss882.jss883.jss885.jss886.jss1572
+
+# // * [ @ id = "downshift-simple-input"]
+# / html / body / div[10] / div[2] / div / div[2] / div / div / div / div[1] / div[1] / div / div / input
+# < input aria - invalid = "false" autocomplete = "off" class = "jss864 jss849 jss2421" id = "downshift-simple-input" name = "off" placeholder = "Start typing to see locations…" required = "" type = "text" value = "" >
+# #downshift-simple-input
+# document.querySelector("#downshift-simple-input")
+# < input aria - invalid = "false" autocomplete = "off" class = "jss864 jss849 jss2421" id = "downshift-simple-input" name = "off"
+# placeholder="Start typing to see locations…" required="" type="text" value="">
