542fbbe57b83e4c8f709b5e4c2f24f5ba94ea7b3
Kevin Bratt <kevinbratt1488@gmail.com>
2021-04-04 09:30:09 -0600

    fixed table stylings a bit

--- csv2pdf.py
+++ csv2pdf.py
@@ -8,17 +8,54 @@ input_pathname = '/Users/kevinbratt/dataloader/'
 
 
 def main(input_file, pathname):
-    columns = ['Case Number', 'Created Time', 'Item Num', 'Item Category',
-               'Item Description', 'Quantity', 'Cost', 'Price']
+    columns = ['Case Number', 'Decedent Name', 'Created Time', 'Item Num',
+               'Item Category', 'Item Description', 'Quantity', 'Cost',
+               'Price']
     df = pd.read_csv(input_pathname + input_file, names=columns)
 
     unique_case_number = df['Case Number'].unique()
     for case in unique_case_number[1:]:
         case_table = df[df['Case Number'] == case]
+        decedent = case_table.iloc[0]['Decedent Name']
 
+        report = '''
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <style>
+                    main {
+                        font-family: "Arial";
+                    }
+                    thead tr {
+                        text-align: center !important;
+                    }
+                    table {
+                        font-family: "Arial";
+                    }
+                    th, td {
+                        border-bottom: 1px solid #ddd !important;
+                    }
+                    tr:hover {
+                        background-color: #f5f5f5;
+                    }
+
+                </style>
+            </head>
+            <main>
+                <div>
+                    %s: %s
+                </div><br>
+                <body>
+                    %s
+                </body>
+            </main>
+        </html>
+        ''' % (case, decedent, case_table.to_html(index=False, border=0))
+    # print(report)
+    # print(decedent)
         filename = pathname + case + '.html'
-        with open(filename, 'w'):
-            case_table.to_html(filename, index=False)
+        with open(filename, 'w') as fo:
+            fo.write(report)
 
         for filepath in glob.iglob(pathname + '*.html'):
             stripped_filename = filepath[:-5]
